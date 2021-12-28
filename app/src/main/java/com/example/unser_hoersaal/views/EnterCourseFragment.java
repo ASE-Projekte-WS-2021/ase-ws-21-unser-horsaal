72eb91eb130fd0557a0cf4eb1affa076d357ed7a
myPhilie <41925280+myPhilie@users.noreply.github.com>
2021-12-28 20:29:14 +0100

    UI + first part of navigation component
    - Created the User Interface for the application
    - Created navigation-graph for navigation component

---
+++ app/src/main/java/com/example/unser_hoersaal/views/EnterCourseFragment.java
@@ -0,0 +1,37 @@
+package com.example.unser_hoersaal.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.unser_hoersaal.R;
+
+public class EnterCourseFragment extends Fragment {
+
+    public EnterCourseFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_enter_course, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
