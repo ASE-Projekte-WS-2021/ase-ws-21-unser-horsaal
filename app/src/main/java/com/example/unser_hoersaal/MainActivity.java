12c341cf9779d15a5060daf2fccd26cce3089148
myPhilie <41925280+myPhilie@users.noreply.github.com>
2021-12-29 15:33:01 +0100

    UI small change
    changed icon for enter or create fragment

--- app/src/main/java/com/example/unser_hoersaal/MainActivity.java
+++ app/src/main/java/com/example/unser_hoersaal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.unser_hoersaal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
