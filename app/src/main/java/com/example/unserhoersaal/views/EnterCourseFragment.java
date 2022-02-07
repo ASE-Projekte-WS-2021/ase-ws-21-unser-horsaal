ae52763d31afab5b73feb320e08b266678c45806
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-02-07 18:41:39 +0100

    Enter-Course-Screen Design Update
    updated Design of Enter-Course-Screen

--- app/src/main/java/com/example/unserhoersaal/views/EnterCourseFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/EnterCourseFragment.java
@@ -1,15 +1,19 @@
 package com.example.unserhoersaal.views;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -19,6 +23,8 @@ import com.example.unserhoersaal.model.DatabaseEnterCourse;
 import com.example.unserhoersaal.utils.KeyboardUtil;
 import com.example.unserhoersaal.viewmodel.CreateCourseViewModel;
 import com.example.unserhoersaal.viewmodel.EnterCourseViewModel;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
 
 /** Fragment for entering a course.*/
 public class EnterCourseFragment extends Fragment {
@@ -27,6 +33,8 @@ public class EnterCourseFragment extends Fragment {
   Button enterCourseButton;
   EnterCourseViewModel enterCourseViewModel;
   CreateCourseViewModel createCourseViewModel;
+  MaterialToolbar toolbar;
+  NavController navController;
 
   public EnterCourseFragment() {
     // Required empty public constructor
@@ -53,16 +61,18 @@ public class EnterCourseFragment extends Fragment {
             .get(CreateCourseViewModel.class);
     initUi(view);
     setupNavigation(view);
+    setupToolbar();
   }
 
   private void initUi(View view) {
     enterCourseEditText = view.findViewById(R.id.enterCourseFragmentCourseNumberEditText);
     enterCourseButton = view.findViewById(R.id.enterCourseFragmentEnterButton);
+    navController = Navigation.findNavController(view);
+    toolbar = view.findViewById(R.id.enterCourseFragmentToolbar);
   }
 
   //setup Navigation to corresponding fragments
   private void setupNavigation(View view) {
-    NavController navController = Navigation.findNavController(view);
     //todo add logic to entering course
     enterCourseButton.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -82,6 +92,13 @@ public class EnterCourseFragment extends Fragment {
         });
         KeyboardUtil.hideKeyboard(getActivity());
       }
+    });
+  }
+
+  private void setupToolbar(){
+    toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+    toolbar.setNavigationOnClickListener(v -> {
+      navController.navigate(R.id.action_enterCourseFragment_to_coursesFragment);
     });
   }
 }
