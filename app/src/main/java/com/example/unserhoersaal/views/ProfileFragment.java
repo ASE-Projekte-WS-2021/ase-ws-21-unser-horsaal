78457311683f18ea802e15784bae56daca8516c1
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-02-07 10:50:53 +0100

    Created Edit-Profile-Screen
    created Screen for editing the profile

--- app/src/main/java/com/example/unserhoersaal/views/ProfileFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/ProfileFragment.java
@@ -22,6 +22,7 @@ import com.example.unserhoersaal.R;
 import com.example.unserhoersaal.utils.KeyboardUtil;
 import com.example.unserhoersaal.viewmodel.LoggedInViewModel;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /** Profile page. */
 public class ProfileFragment extends Fragment {
@@ -30,6 +31,7 @@ public class ProfileFragment extends Fragment {
   private MaterialToolbar toolbar;
   private NavController navController;
   private MenuItem logout;
+  private FloatingActionButton fab;
 
   private LoggedInViewModel loggedInViewModel;
 
@@ -64,6 +66,10 @@ public class ProfileFragment extends Fragment {
     toolbar = view.findViewById(R.id.profileFragmentToolbar);
     navController = Navigation.findNavController(view);
     logout = view.findViewById(R.id.profileFragmentToolbarLogout);
+    fab = view.findViewById(R.id.profileFragmentFab);
+    fab.setOnClickListener(v -> {
+      navController.navigate(R.id.action_profileFragment_to_editProfileFragment);
+    });
   }
 
   private void initToolbar(){
