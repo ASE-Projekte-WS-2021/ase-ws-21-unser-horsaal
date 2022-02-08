274e280200b88b902a18d736f7559533f223146f
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-02-08 11:29:02 +0100

    profile-screen update
    - added password-toggle

--- app/src/main/java/com/example/unserhoersaal/views/ProfileFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/ProfileFragment.java
@@ -1,18 +1,29 @@
 package com.example.unserhoersaal.views;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
 import com.example.unserhoersaal.R;
 import com.example.unserhoersaal.viewmodel.LoginRegisterViewModel;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,12 +33,16 @@ public class ProfileFragment extends Fragment {
 
   private static final String TAG = "ProfileFragment";
 
-  private EditText userEmailEditText;
-  private EditText userPasswordEditText;
+  private TextView userEmail;
+  private EditText userPassword;
   private Button editProfileButton;
-  private Button logoutButton;
-
+  private ImageView togglePasswordIcon;
+  private boolean passwordMask;
   private NavController navController;
+  private MenuItem logout;
+  private FloatingActionButton fab;
+  private MaterialButton deleteAccountButton;
+  private MaterialToolbar toolbar;
 
   private LoginRegisterViewModel loginRegisterViewModel;
 
@@ -37,6 +52,7 @@ public class ProfileFragment extends Fragment {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
   }
 
@@ -52,7 +68,9 @@ public class ProfileFragment extends Fragment {
     super.onViewCreated(view, savedInstanceState);
     this.initViewModel();
     this.initUi(view);
-    this.initLogoutButton(view);
+    this.initToolbar();
+    this.initLogout(view);
+    this.passwordMask = true;
   }
 
   private void initViewModel() {
@@ -71,21 +89,44 @@ public class ProfileFragment extends Fragment {
   }
 
   private void initUi(View view) {
-    this.userEmailEditText = view.findViewById(R.id.profileFragmentUserEmail);
-    this.userPasswordEditText = view.findViewById(R.id.profileFragmentExamplePassword);
+    this.userEmail = view.findViewById(R.id.profileFragmentUserEmail);
+    this.userPassword = view.findViewById(R.id.profileFragmentExamplePassword);
+    this.togglePasswordIcon = view.findViewById(R.id.profileFragmentTogglePasswordIcon);
     this.editProfileButton = view.findViewById(R.id.profileFragmentEditProfileButton);
-    this.logoutButton = view.findViewById(R.id.profileFragmentLogoutButton);
+    this.toolbar = view.findViewById(R.id.profileFragmentToolbar);
+    this.navController = Navigation.findNavController(view);
+    this.logout = view.findViewById(R.id.profileFragmentToolbarLogout);
+    this.deleteAccountButton = view.findViewById(R.id.profileFragmentDeleteAccountButton);
+    this.fab = view.findViewById(R.id.profileFragmentFab);
+    this.fab.setOnClickListener(v -> {
+      navController.navigate(R.id.action_profileFragment_to_editProfileFragment);
+    });
+    this.togglePasswordIcon.setOnClickListener(v -> {
+      this.togglePassword();
+    });
   }
 
   /** This method initializes the logout button. */
-  public void initLogoutButton(View view) {
+  public void initLogout(View view) {
     this.navController = Navigation.findNavController(view);
 
-    this.logoutButton.setOnClickListener(new View.OnClickListener() {
+    this.logout.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         loginRegisterViewModel.logOut();
       }
     });
+  }
+
+  private void togglePassword(){
+    if (this.passwordMask){
+      this.userPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+      this.togglePasswordIcon.setColorFilter(getResources().getColor(R.color.app_blue));
+      this.passwordMask = false;
+    }else {
+      this.userPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+      this.togglePasswordIcon.setColorFilter(getResources().getColor(R.color.grey));
+      this.passwordMask = true;
+    }
   }
 }
