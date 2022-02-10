a418eca6c9c424226e04cab3471110a7ad31790c
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-02-10 20:06:03 +0100

    Added layout for creating a thread

--- app/src/main/java/com/example/unserhoersaal/views/CourseMeetingFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/CourseMeetingFragment.java
@@ -1,9 +1,12 @@
 package com.example.unserhoersaal.views;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +16,9 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.unserhoersaal.R;
+import com.example.unserhoersaal.utils.KeyboardUtil;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /** Course-Meeting. */
@@ -25,6 +30,10 @@ public class CourseMeetingFragment extends Fragment {
   RecyclerView threadRecycler;
   FloatingActionButton floatingActionButton;
   NavController navController;
+  ScrollView createThreadContainer;
+  EditText createThreadTitle;
+  EditText createThreadText;
+  MaterialButton sendThreadButton;
 
   public CourseMeetingFragment() {
     // Required empty public constructor
@@ -56,7 +65,14 @@ public class CourseMeetingFragment extends Fragment {
             .findViewById(R.id.courseMeetingFragmentGeneralThreadContributions);
     threadRecycler = view.findViewById(R.id.courseMeetingFragmentThreadRecycler);
     floatingActionButton = view.findViewById(R.id.courseMeetingFragmentFab);
+    floatingActionButton.setOnClickListener(v -> {
+      onFloatingActionButtonClicked();
+    });
     navController = Navigation.findNavController(view);
+    createThreadContainer = view.findViewById(R.id.courseMeetingFragmentCreateThreadContainer);
+    createThreadTitle = view.findViewById(R.id.courseMeetingFragmentQuestionTitleEditText);
+    createThreadText = view.findViewById(R.id.courseMeetingFragmentQuestionTextEditText);
+    sendThreadButton = view.findViewById(R.id.courseMeetingFragmentSendThreadButton);
   }
 
   private void initToolbar() {
@@ -64,5 +80,22 @@ public class CourseMeetingFragment extends Fragment {
     toolbar.setNavigationOnClickListener(v -> {
 
     });
+  }
+
+  private void onFloatingActionButtonClicked() {
+    if (createThreadContainer.getVisibility() == View.VISIBLE) {
+      KeyboardUtil.hideKeyboard(getActivity());
+      createThreadContainer.setVisibility(View.GONE);
+      floatingActionButton.setImageResource(R.drawable.ic_baseline_add_24);
+      floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(getResources()
+              .getColor(R.color.orange, null)));
+      createThreadTitle.getText().clear();
+      createThreadText.getText().clear();
+    } else {
+      createThreadContainer.setVisibility(View.VISIBLE);
+      floatingActionButton.setImageResource(R.drawable.ic_baseline_close_24);
+      floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(getResources()
+              .getColor(R.color.red, null)));
+    }
   }
 }
