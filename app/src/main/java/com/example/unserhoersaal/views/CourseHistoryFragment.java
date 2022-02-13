ddb6c63c3c37f9d28fbc939d6d7459a8210bf06e
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-02-13 08:51:29 +0100

    Course-Description Design update
    changed layout to tablelayout

--- app/src/main/java/com/example/unserhoersaal/views/CourseHistoryFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/CourseHistoryFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ScrollView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
