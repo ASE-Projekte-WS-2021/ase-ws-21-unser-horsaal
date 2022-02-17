206ebd260eaf19a92479abf620886983f6e17dbe
Rusi91 <ruslanasabidi@gmail.com>
2022-02-17 15:41:42 +0100

    adjustements

--- app/src/main/java/com/example/unserhoersaal/views/LoginFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/LoginFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
