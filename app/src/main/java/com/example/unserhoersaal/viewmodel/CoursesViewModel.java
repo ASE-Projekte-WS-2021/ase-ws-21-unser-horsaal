7e2f710d8c2a5e275f7623c08e38a69a5a19d9d1
Rusi91 <ruslanasabidi@gmail.com>
2022-02-18 12:04:25 +0100

    Revert "Merge branch 'dev' of https://github.com/ASE-Projekte-WS-2021/ase-ws-21-unser-horsaal into dev"
    This reverts commit c6ceaf81cfb316ed359aaec03b2c898991c96efe, reversing
    changes made to e72d332df7a704b438e8665bb962e90ae56f18d4.

--- app/src/main/java/com/example/unserhoersaal/viewmodel/CoursesViewModel.java
+++ app/src/main/java/com/example/unserhoersaal/viewmodel/CoursesViewModel.java
@@ -1,7 +1,5 @@
 package com.example.unserhoersaal.viewmodel;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
