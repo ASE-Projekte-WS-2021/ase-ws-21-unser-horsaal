f23c5ec05acff0b26f11c4142189da62e70f05ca
vairasza <vairasza@mailbox.org>
2022-02-26 21:52:48 +0100

    first iteration enter course fragment

--- app/src/main/java/com/example/unserhoersaal/views/EnterCourseDialogFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/EnterCourseDialogFragment.java
@@ -1,90 +1,60 @@
-package com.example.unserhoersaal.views;
+/*package com.example.unserhoersaal.views;
 
-import android.app.Dialog;
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
-import androidx.lifecycle.ViewModelProvider;
 import com.example.unserhoersaal.R;
-import com.example.unserhoersaal.model.CourseModel;
+import com.example.unserhoersaal.databinding.EnterCourseDetailScreenBinding;
 import com.example.unserhoersaal.viewmodel.EnterCourseViewModel;
-import com.google.android.material.button.MaterialButton;
 
-/** Dialog that contains Course-Information and option to enter the course. Source:
- * https://stackoverflow.com/questions/25887373/calling-dialogfragment-from-fragment-not-
- * fragmentactivity.*/
-public class EnterCourseDialogFragment extends DialogFragment {
+
+ Dialog that contains Course-Information and option to enter the course. Source:
+  https://stackoverflow.com/questions/25887373/calling-dialogfragment-from-fragment-not-
+  fragmentactivity.
+public class EnterCourseDialogFragment extends Fragment {
 
   private static final String TAG = "EnterCourseDialogFragment";
 
-  private View view;
-
-  private MaterialButton cancelButton;
-  private MaterialButton enterButton;
-  private TextView courseTitle;
-  private TextView courseCreator;
-  private TextView courseInstitution;
-  private TextView courseDescription;
-
   private EnterCourseViewModel enterCourseViewModel;
+  private EnterCourseDetailScreenBinding binding;
 
-  public EnterCourseDialogFragment() {
+  public EnterCourseDialogFragment() {}
 
-  }
-
-  @NonNull
   @Override
-  public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-    this.initViewModel();
-    this.initUi();
-    this.cancelButton.setOnClickListener(v -> dismiss());
-    this.enterButton.setOnClickListener(v -> enter());
-    Dialog builder = new Dialog(getActivity());
-    builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    builder.setContentView(this.view);
-    builder.setCancelable(true);
-    return builder;
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
   }
 
-  /** Initialize all ViewModels for the fragment. */
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    this.binding =  DataBindingUtil.inflate(inflater,
+            R.layout.enter_course_detail_screen, container,false);
+    return this.binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    this.initViewModel();
+    this.connectBinding();
+  }
+
   public void initViewModel() {
     this.enterCourseViewModel = new ViewModelProvider(requireActivity())
             .get(EnterCourseViewModel.class);
     this.enterCourseViewModel.init();
   }
 
-  private void initUi() {
-    this.view = getActivity().getLayoutInflater().inflate(R.layout.enter_course_detail_screen, new
-            LinearLayout(getActivity()), false);
-
-    this.cancelButton = this.view.findViewById(R.id.enterCourseDialogDeclineButton);
-    this.enterButton = this.view.findViewById(R.id.enterCourseDialogEnterCourseButton);
-    this.courseTitle = this.view.findViewById(R.id.enterCourseDialogCourseTitle);
-    this.courseCreator = this.view.findViewById(R.id.enterCourseDialogCourseCreator);
-    this.courseInstitution = this.view.findViewById(R.id.enterCourseDialogCourseInstitution);
-    this.courseDescription = this.view.findViewById(R.id.enterCourseDialogCourseDescription);
-    this.setContent(enterCourseViewModel.getCourse().getValue());
+  private void connectBinding() {
+    this.binding.setLifecycleOwner(getViewLifecycleOwner());
+    this.binding.setVm(this.enterCourseViewModel);
   }
 
-  /**Comment.*/
-  public void setContent(CourseModel course) {
-    this.courseTitle.setText(course.getTitle());
-    this.courseCreator.setText(course.getCreatorId());
-    this.courseInstitution.setText(course.getInstitution());
-    //TODO ALTERNATIVE FOR TOO LONG STRING
-    String maxCharacterString = course.getDescription()
-            .substring(0, Math.min(course.getDescription().length(), 200));
-    this.courseDescription.setText(maxCharacterString);
-  }
-
-  public void enter() {
-    this.enterCourseViewModel.enterCourse();
-    dismiss();
-  }
 }
+*/
