19346455c8f8691c33e1063ad7930f744073e84b
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-03-03 17:50:33 +0100

    StatusBar Design-Update
    - added color gradient to statusbar
    - NOTE: also adds gradient to system navigation

--- app/src/main/java/com/example/unserhoersaal/MainActivity.java
+++ app/src/main/java/com/example/unserhoersaal/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.unserhoersaal;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -20,9 +29,20 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setStatusBarGradiant();
     setContentView(R.layout.activity_main);
     if (getSupportActionBar() != null) {
       getSupportActionBar().hide();
     }
   }
-}
+
+  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+  public void setStatusBarGradiant() {
+    Window window = this.getWindow();
+    Drawable background = ResourcesCompat.getDrawable(getResources(), R.drawable.app_background, null);
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    window.setStatusBarColor(this.getResources().getColor(android.R.color.transparent));
+    window.setNavigationBarColor(this.getResources().getColor(android.R.color.transparent));
+    window.setBackgroundDrawable(background);
+    }
+  }
