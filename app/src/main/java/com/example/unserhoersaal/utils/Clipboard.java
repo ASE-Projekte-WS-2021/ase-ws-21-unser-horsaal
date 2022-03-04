d318396201572e319f25e1306f96bad387d20d90
vairasza <vairasza@mailbox.org>
2022-03-04 12:24:11 +0100

    bugfix clipboard

--- app/src/main/java/com/example/unserhoersaal/utils/Clipboard.java
+++ app/src/main/java/com/example/unserhoersaal/utils/Clipboard.java
@@ -13,8 +13,8 @@ public class Clipboard {
   @BindingAdapter("copyToClipboard")
   public static void copyToClipboard(View view, String text) {
     ClipboardManager clipboardManager = (ClipboardManager) view.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-    String deepLink = Config.DEEP_LINK_URL + Config.COURSE_CODE_MAPPING_CLIPBOARD;
-    ClipData clipData = ClipData.newPlainText(deepLink, text);
+    String deepLink = Config.DEEP_LINK_URL + text;
+    ClipData clipData = ClipData.newPlainText(Config.COURSE_CODE_MAPPING_CLIPBOARD, deepLink);
     clipboardManager.setPrimaryClip(clipData);
 
     Toast.makeText(view.getContext(), Config.COURSE_CODE_MAPPING_CLIPBOARD_TOAST_TEXT, Toast.LENGTH_SHORT).show();
