2b1ecc726095750286edfc8cd35358a3060e8206
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-03-14 17:03:39 +0100

    CreateThreadFragment Update
    - databinding
    - methods for toggling visibility of hashtag and pagenumber

--- app/src/main/java/com/example/unserhoersaal/utils/VisibilityAdapter.java
+++ app/src/main/java/com/example/unserhoersaal/utils/VisibilityAdapter.java
@@ -32,4 +32,16 @@ public class VisibilityAdapter {
     }
   }
 
+  /** Toggles Hashtag/PageNumber Editfield in Create Thread Fragment */
+  @BindingAdapter("toggleEditField")
+  public static void toggleEditField(View editField, View arrow) {
+    if (editField.getVisibility() == View.VISIBLE) {
+      editField.setVisibility(View.GONE);
+      arrow.animate().rotationBy(-90);
+    } else {
+      editField.setVisibility(View.VISIBLE);
+      arrow.animate().rotationBy(90);
+    }
+  }
+
 }
--- app/src/main/java/com/example/unserhoersaal/views/CreateThreadFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/CreateThreadFragment.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.unserhoersaal.R;
+import com.example.unserhoersaal.databinding.FragmentCreateThreadBinding;
 
 /**Create Thread.*/
 public class CreateThreadFragment extends Fragment {
+
+  private FragmentCreateThreadBinding binding;
+  private NavController navController;
 
   public CreateThreadFragment() {
     // Required empty public constructor
@@ -27,12 +34,30 @@ public class CreateThreadFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_create_thread, container, false);
+    this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_thread, container,
+            false);
+    return this.binding.getRoot();
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+
+    this.navController = Navigation.findNavController(view);
+
+    this.connectBinding();
+    this.initToolbar();
+  }
+
+  private void connectBinding() {
+    this.binding.setLifecycleOwner(getViewLifecycleOwner());
+  }
+
+  private void initToolbar() {
+    this.binding.createThreadFragmentToolbar
+            .setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+    this.binding.createThreadFragmentToolbar
+            .setNavigationOnClickListener(v ->
+                    navController.navigate(R.id.action_createThreadFragment_to_courseMeetingFragment));
   }
 }
