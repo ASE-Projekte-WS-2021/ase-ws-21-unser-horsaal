111e66548cb2669e7fc71b00be0d8ff122235c6b
Rusi91 <ruslanasabidi@gmail.com>
2022-03-18 13:47:06 +0100

    Revert "Merge branch 'dev_course_area_adjustments' into dev"
    This reverts commit fcf6182ee0a19c30328de8fc1fe19068175f3bbd, reversing
    changes made to 86b8b266d505bb29d5395dedf6cc5965bec87654.

--- app/src/main/java/com/example/unserhoersaal/viewmodel/CourseMeetingViewModel.java
+++ app/src/main/java/com/example/unserhoersaal/viewmodel/CourseMeetingViewModel.java
@@ -14,9 +14,6 @@ import com.example.unserhoersaal.utils.StateLiveData;
 import com.example.unserhoersaal.utils.Validation;
 import java.util.Collections;
 import java.util.Comparator;
-import com.example.unserhoersaal.utils.ArrayListUtil;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /** ViewModel for the CourseMeetingFragment. */
@@ -25,7 +22,6 @@ public class CourseMeetingViewModel extends ViewModel {
   private static final String TAG = "CourseMeetingViewModel";
 
   private CourseMeetingRepository courseMeetingRepository;
-  private ArrayListUtil arrayListUtil = new ArrayListUtil();
 
   private StateLiveData<MeetingsModel> meeting = new StateLiveData<>();
   private StateLiveData<List<ThreadModel>> threads;
@@ -39,7 +35,6 @@ public class CourseMeetingViewModel extends ViewModel {
     }
 
     this.courseMeetingRepository = CourseMeetingRepository.getInstance();
-    this.arrayListUtil = new ArrayListUtil();
     this.meeting = this.courseMeetingRepository.getMeeting();
     this.threadModelMutableLiveData =
             this.courseMeetingRepository.getThreadModelMutableLiveData();
@@ -54,26 +49,14 @@ public class CourseMeetingViewModel extends ViewModel {
     return this.threads;
   }
 
-  /** sort the threads list.
-   *  First parameter is the treads list to sort.
-   *  The second parameter is a sort option (String).
-   *  Sort options: "newest", "likes" and "answers"
-   */
-  public void sortThreads(List<ThreadModel> threadsModelList, String sortOption) {
-    this.arrayListUtil.sortThreadList(threadsModelList, sortOption);
-  }
-
-  /** filter the threads list.
-   *  First parameter is the treads list to filter.
-   *  The second parameter is a filter option (String).
-   *  filter options: "answered" and "not answered"
-   */
-  public void filterThreads(List<ThreadModel> threadsModelList, String filterOption) {
-    MeetingsModel actualMeeting = this.meeting.getValue();
-    List<ThreadModel> fullThreadsModelList = new ArrayList<>(this.threads.getValue());
-    String userId = courseMeetingRepository.getUserId();
-    this.arrayListUtil.filterThreadList(threadsModelList, fullThreadsModelList, filterOption,
-            actualMeeting, userId);
+  /** Sort the threads list by likes. */
+  public void sortThreadsByLikes(List<ThreadModel> threadsModelList) {
+    Collections.sort(threadsModelList, new Comparator<ThreadModel>() {
+      @Override
+      public int compare(ThreadModel threadModel, ThreadModel t1) {
+        return t1.getLikes() - threadModel.getLikes();
+      }
+    });
   }
 
   public StateLiveData<MeetingsModel> getMeeting() {
