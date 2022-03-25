c2311d32ec18d0fcd051972b7837ae6f1e4285ee
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-03-25 17:00:06 +0100

    Live-Chat UI
    - added UI for Live-Chat
    - added UI for Live-Chat-Item

---
+++ app/src/main/java/com/example/unserhoersaal/views/LiveChatFragment.java
@@ -0,0 +1,38 @@
+package com.example.unserhoersaal.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.unserhoersaal.R;
+
+/**Live-Chat.*/
+public class LiveChatFragment extends Fragment {
+
+  public LiveChatFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_live_chat, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+  }
+}
