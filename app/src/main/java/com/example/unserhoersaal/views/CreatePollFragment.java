775911d59f02373382ca8a0b000cf3fd0d5189de
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-03-26 12:36:21 +0100

    Create-Poll UI
    - added UI for creating poll

---
+++ app/src/main/java/com/example/unserhoersaal/views/CreatePollFragment.java
@@ -0,0 +1,38 @@
+package com.example.unserhoersaal.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.unserhoersaal.R;
+
+/**Create poll.*/
+public class CreatePollFragment extends Fragment {
+
+  public CreatePollFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_create_poll, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+  }
+}
