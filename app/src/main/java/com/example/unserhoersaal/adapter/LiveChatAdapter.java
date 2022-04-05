68674076bf7da43e1ae78cf4cab6cea89ba3a87e
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-04-05 12:57:20 +0200

    LiveChat Scroll Indicator
    - shows bubble but no content

--- app/src/main/java/com/example/unserhoersaal/adapter/LiveChatAdapter.java
+++ app/src/main/java/com/example/unserhoersaal/adapter/LiveChatAdapter.java
@@ -1,26 +1,33 @@
 package com.example.unserhoersaal.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.unserhoersaal.R;
 import com.example.unserhoersaal.databinding.LiveChatItemBinding;
 import com.example.unserhoersaal.model.LiveChatMessageModel;
 import com.example.unserhoersaal.viewmodel.LiveChatViewModel;
+import com.l4digital.fastscroll.FastScroller;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /** Chatadapter. */
-public class LiveChatAdapter extends RecyclerView.Adapter<LiveChatAdapter.ViewHolder> {
+public class LiveChatAdapter extends RecyclerView.Adapter<LiveChatAdapter.ViewHolder>
+        implements FastScroller.SectionIndexer{
 
   private static final String TAG = "ChatAdapter";
 
   private final List<LiveChatMessageModel> localDataSet;
   private final LiveChatViewModel liveChatViewModel;
+  private LiveChatMessageModel visibleItem;
 
   public LiveChatAdapter(List<LiveChatMessageModel> dataSet, LiveChatViewModel liveChatViewModel) {
     this.localDataSet = dataSet;
@@ -47,6 +54,42 @@ public class LiveChatAdapter extends RecyclerView.Adapter<LiveChatAdapter.ViewHo
   @Override
   public int getItemCount() {
     return this.localDataSet.size();
+  }
+
+
+  /**Source: https://stackoverflow.com/questions/24989218/get-visible-items-in-recyclerview.*/
+  @Override
+  public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+    super.onAttachedToRecyclerView(recyclerView);
+    RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+    if (manager instanceof LinearLayoutManager && getItemCount() > 0) {
+      LinearLayoutManager linearLayoutManager = (LinearLayoutManager) manager;
+      recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+          super.onScrollStateChanged(recyclerView, newState);
+        }
+
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+          super.onScrolled(recyclerView, dx, dy);
+          int visiblePosition = linearLayoutManager.findFirstVisibleItemPosition();
+          if (visiblePosition == )
+          if (visiblePosition > -1) {
+            visibleItem = localDataSet.get(visiblePosition);
+          }
+        }
+      });
+    }
+  }
+
+  @Override
+  public CharSequence getSectionText(int position) {
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+    if (visibleItem == null) {
+      return null;
+    }
+    return sdf.format(this.visibleItem.getCreationTime());
   }
 
   /** Viewholder. */
--- app/src/main/java/com/example/unserhoersaal/views/LiveChatFragment.java
+++ app/src/main/java/com/example/unserhoersaal/views/LiveChatFragment.java
@@ -72,6 +72,7 @@ public class LiveChatFragment extends Fragment {
     this.initViewModel();
     this.connectAdapter();
     this.connectBinding();
+    this.setupScrolling();
   }
 
   @SuppressLint("NotifyDataSetChanged")
@@ -148,5 +149,20 @@ public class LiveChatFragment extends Fragment {
   public void onDestroy() {
     super.onDestroy();
     KeyboardUtil.hideKeyboard(getActivity());
+  }
+
+  private void setupScrolling() {
+    View infoContainer = this.binding.liveChatFragmentInfoContainer;
+    this.binding.liveChatFragmentChatRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+      @Override
+      public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+        if (!recyclerView.canScrollVertically(-1)) {
+          infoContainer.setVisibility(View.VISIBLE);
+        } else {
+          infoContainer.setVisibility(View.GONE);
+        }
+      }
+    });
   }
 }
