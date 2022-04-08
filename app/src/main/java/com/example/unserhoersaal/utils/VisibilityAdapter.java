56259935f4a564911393b3dfdc2e076277e5286c
myPhilie <41925280+myPhilie@users.noreply.github.com>
2022-04-08 13:34:17 +0200

    checkstyles and problems
    - in utility classes and config

--- app/src/main/java/com/example/unserhoersaal/utils/VisibilityAdapter.java
+++ app/src/main/java/com/example/unserhoersaal/utils/VisibilityAdapter.java
@@ -6,36 +6,10 @@ import androidx.databinding.BindingAdapter;
 /** Show/Hide Floating Buttons. */
 public class VisibilityAdapter {
 
-  /** Opens Floating Buttons in Course Fragment Fragment. */
-  @BindingAdapter("openCreationLayout")
-  public static void openCreationLayout(View floatButton, View creationLayout) {
-    floatButton.setVisibility(View.GONE);
-    creationLayout.setVisibility(View.VISIBLE);
-  }
-
-  /** Closes Floating Buttons in Course Fragment Fragment. */
-  @BindingAdapter("closeCreationLayout")
-  public static void closeCreationLayout(View floatButton, View creationLayout) {
-    floatButton.setVisibility(View.VISIBLE);
-    creationLayout.setVisibility(View.GONE);
-  }
-
-  /** Toggles Floating Buttons in Meeting Fragment. */
-  @BindingAdapter("toggleFloatingButtons")
-  public static void toggleFloatingButtons(View floatButton1, View floatButton2) {
-    if (floatButton1.getVisibility() == View.VISIBLE) {
-      floatButton1.setVisibility(View.GONE);
-      floatButton2.setVisibility(View.GONE);
-    } else {
-      floatButton1.setVisibility(View.VISIBLE);
-      floatButton2.setVisibility(View.VISIBLE);
-    }
-  }
-
   /** Toggles Hashtag/PageNumber Editfield in Create Thread Fragment. */
   @BindingAdapter("toggleEditField")
   public static void toggleEditField(View editField, View arrow) {
-    if(PreventDoubleClick.checkIfDoubleClick()) {
+    if (PreventDoubleClick.checkIfDoubleClick()) {
       return;
     }
     if (editField.getVisibility() == View.VISIBLE) {
